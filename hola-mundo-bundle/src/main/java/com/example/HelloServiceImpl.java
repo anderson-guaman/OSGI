4b842e75bf4c4b741f20e1fceb0f5b2b19079ed0
anderson-guaman <132541127+anderson-guaman@users.noreply.github.com>
2024-07-02 17:16:01 -0500

    Add files via upload
    PRACTICA OSGI FUNCIONANDO

---
+++ hola-mundo-bundle/src/main/java/com/example/HelloServiceImpl.java
@@ -0,0 +1,9 @@
+package com.example;
+
+public class HelloServiceImpl implements com.example.HelloService {
+
+    @Override
+    public String sayHello() {
+        return "Hola mundo! Soy Anderson Guaman";
+    }
+}
